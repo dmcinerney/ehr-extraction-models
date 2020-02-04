e3e5159bf8ad2ad71e5c83046098e10aed79de4a
Jered McInerney <jered.mcinerney@gmail.com>
2020-02-04 18:59:57 -0500

    changed parameter path

--- parameters.py
+++ parameters.py
@@ -1,1 +1,1 @@
-pretrained_model = '/home/jered/Documents/projects/clinical-bert/clinical-bert-weights/model (1)/model/pretraining'
+pretrained_model = '/home/jered/Documents/projects/clinical-bert/clinical-bert-weights'
