1741682ff5ddda166f2388e8dcf9ce498d5c2406
Jered McInerney <mcinerney.de@husky.neu.edu>
2020-03-05 19:13:20 -0500

    started training final models with these parameters

--- parameters.py
+++ parameters.py
@@ -1,19 +1,19 @@
 pretrained_model = '/home/jered/Documents/projects/clinical-bert/clinical-bert-weights'
 code_graph_file = '/home/jered/Documents/data/icd_codes/code_graph_radiology_expanded.pkl'
-#data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/reports_and_codes_expanded'
-data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/mini'
+data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/reports_and_codes_expanded'
+#data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/mini'
 #data_dir = '/home/jered/Documents/data/mimic-iii-clinical-database-1.4/preprocessed/reports_and_codes_expanded'
 #data_dir = '/home/jered/Documents/data/mimic-iii-clinical-database-1.4/preprocessed/mini'
 
 # training params
 batch_size = 8
-epochs = 4
-limit_rows_train = None
-limit_rows_val = None
+epochs = 2
+limit_rows_train = 10000
+limit_rows_val = 1000
 subbatches = 4
 num_workers = 0
 checkpoint_every = 10
-copy_checkpoint_every = 100
+copy_checkpoint_every = 300
 val_every = 10
 email_every = 30
 sender_email = 'jeredspython@gmail.com'
--- train_models.sh
+++ train_models.sh
@@ -2,7 +2,7 @@
 
 read -s -p "Enter Password: " pswd
 echo
-checkpoints="checkpoints_temp"
+checkpoints="checkpoints"
 mkdir "${checkpoints}/code_supervision_only_description_unfrozen"
 python train.py code_supervision_only_description_unfrozen --save_checkpoint_folder "${checkpoints}/code_supervision_only_description_unfrozen" --password $pswd
 mkdir "${checkpoints}/code_supervision_only_linearization_description_unfrozen"
