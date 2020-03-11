19bf4244328de01ff6c4cee97dd460962c9c244d
Jered McInerney <jered.mcinerney@gmail.com>
2020-03-11 16:18:44 -0400

    more parameter options

--- parameters.py
+++ parameters.py
@@ -1,16 +1,18 @@
 pretrained_model = '/home/jered/Documents/projects/clinical-bert/clinical-bert-weights'
 code_graph_file = '/home/jered/Documents/data/icd_codes/code_graph_radiology_expanded.pkl'
-data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/reports_and_codes_expanded'
+#data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/reports_and_codes_expanded'
 #data_dir = '/home/jered/Documents/data/Dataset_10-11-2019/preprocessed/mini'
-#data_dir = '/home/jered/Documents/data/mimic-iii-clinical-database-1.4/preprocessed/reports_and_codes_expanded'
+data_dir = '/home/jered/Documents/data/mimic-iii-clinical-database-1.4/preprocessed/reports_and_codes_expanded'
 #data_dir = '/home/jered/Documents/data/mimic-iii-clinical-database-1.4/preprocessed/mini'
 
 # training params
 batch_size = 8
-epochs = 2
-limit_rows_train = 10000
-limit_rows_val = 1000
-subbatches = 4
+epochs = 5
+#limit_rows_train = 10000
+limit_rows_train = None
+#limit_rows_val = 1000
+limit_rows_val = None
+subbatches = 8
 num_workers = 0
 checkpoint_every = 10
 copy_checkpoint_every = 300
@@ -18,7 +20,8 @@ val_every = 10
 email_every = 30
 
 #  model params
-sentences_per_checkpoint = 13
+#sentences_per_checkpoint = 13
+sentences_per_checkpoint = 17
 
 # email params
 smtp_server = 'smtp.gmail.com'
