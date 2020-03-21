e6fcfa8a4faff6eccc937067a302bd9e95ed70c0
Jered McInerney <jered.mcinerney@gmail.com>
2020-03-21 17:45:01 -0400

    modified parameters to add learning rate

--- model_loader.py
+++ model_loader.py
@@ -16,47 +16,47 @@ model_components = {
     'code_supervision': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_id=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=True),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_frozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_unfrozen': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_id=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=False, dropout=0),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.00001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_unfrozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_only_linearization': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_linearization=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=True),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_frozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_only_linearization_unfrozen': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_linearization=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=False, dropout=0),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.00001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_unfrozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_only_description': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_description=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=True),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_frozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_only_description_unfrozen': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_description=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=False, dropout=0),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.00001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_unfrozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_only_linearization_description_unfrozen': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, description_linearization=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=False, dropout=0),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.00001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_unfrozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB if run_type == 'training' else (OBT if run_type == 'testing' else OBA)))},
     'code_supervision_individual_sentence': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_id=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model_is(sentences_per_checkpoint=sentences_per_checkpoint, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params()),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_frozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB_is if run_type == 'training' else (OBT_is if run_type == 'testing' else OBA_is)))},
     'code_supervision_individual_sentence_unfrozen': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, resample_neg_proportion=.01 if run_type == 'training' else None, counts=counts, code_id=True),
         'model_class': lambda device, batcher, cluster, sentences_per_checkpoint:Model_is(sentences_per_checkpoint=17, device1=device, device2='cpu', cluster=cluster, code_embedding_type_params=batcher.get_code_embedding_type_params(), concatenate_code_embedding=p.concatenate_code_embedding, freeze_bert=False, dropout=0),
-        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=.00001),
+        'optimizer_class': lambda parameters: torch.optim.Adam(parameters, lr=p.learning_rate_unfrozen),
         'postprocessor': lambda batcher, run_type: Postprocessor(batcher.hierarchy, batcher.code_idxs, (OB_is if run_type == 'training' else (OBT_is if run_type == 'testing' else OBA_is)))},
     'cosine_similarity': {
         'batcher_class': lambda hierarchy, counts, run_type: Batcher(hierarchy, code_description=True, add_special_tokens=False),
